2ffa6c4c1c50781c8f28c470cde1e3ecaf1882a3
DevOpsStudent-18 <karthikagitmail1989@gmail.com>
2025-12-31 17:34:01 +0530

    Create Checksite.py

---
+++ Checksite.py
@@ -0,0 +1,15 @@
+
+import requests
+
+def check_website_status(url):
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            print(f" The website '{url}' is up and running!")
+        else:
+            print(f" The website '{url}' returned status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f" The website '{url}' is down or unreachable. Error: {e}")
+
+
+check_website_status("https://www.google.com/")
